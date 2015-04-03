471e7acbe446dd8e844b55800110dc436dac8c12
NomNuggetNom <nomnuggetnom@gmail.com>
2015-04-03 17:53:44 -0400

    Create whitelist-tables.js

---
+++ assets/javascripts/whitelist-tables.js
@@ -0,0 +1,7 @@
+(function() {
+  Discourse.Markdown.whiteListTag('table');
+  Discourse.Markdown.whiteListTag('tr');
+  Discourse.Markdown.whiteListTag('th');
+  Discourse.Markdown.whiteListTag('td');
+  Discourse.Markdown.whiteListTag('caption');
+})();
