78b564a9fd27d6a4cc2c1492b4aa6242146d2e6c
NomNuggetNom <nomnuggetnom@gmail.com>
2015-04-03 17:52:15 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: fontawesome
+# about: a plugin that allows for insertion of fontawesome icons
+# version: 0.1
+# authors: NomNuggetNom
+
+register_asset "javascripts/whitelist-tables.js", :server_side
